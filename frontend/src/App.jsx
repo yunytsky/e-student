4045162abf015f3f5e3d36695550358d475082b4
yunytsky <cybersoulregistration@protonmail.com>
2023-10-23 13:56:51 +0300

    created: router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
+import { RouterProvider, createBrowserRouter, Route, createRoutesFromElements } from "react-router-dom"
+
+import Main from "./pages/www/Main"
+import Login from "./pages/www/Login"
+
+const router = createBrowserRouter(
+  createRoutesFromElements((
+    <Route path="/">
+      <Route index element={<Main/>}/>
+      <Route path="login" element={<Login/>}/>
+      <Route path="signup"/>
+
+      <Route path="app">
+        <Route path="profile"/>
+      </Route>
+    </Route>
+  ))
+)
+
 function App() {
   return (
     <div className="App">
-     
+      <RouterProvider router={router}/>
     </div>
   )
 }
