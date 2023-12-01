90fe6c8ca3fb0c1a7868e865acbd613aac20a9d7
VecroKatas <tymur.katas@gmail.com>
2023-12-01 19:07:31 +0200

    json return
    Changed JWT exporation to 1 day. Changed profile endpoints to ..../user/...-profile

--- backend/Models/DormResidentConstants.cs
+++ backend/Models/DormResidentConstants.cs
@@ -4,7 +4,7 @@ public class DormResidentConstants
 {
     public static List<DormResidentModel> DormResidents = new List<DormResidentModel>()
     {
-        new DormResidentModel(){FullName = "First Real Name", DormAddress = "Сєчєнова, 6", 
+        new DormResidentModel(){FullName = "Петро Порох", DormAddress = "Сєчєнова, 6", 
             DormAccount = "11111111", DormPassNumber = "123456", DormRoom = "101/2"},
         new DormResidentModel(){FullName = "Third Real Name", DormAddress = "Сєчєнова, 6", 
             DormAccount = "33333333", DormPassNumber = "345678", DormRoom = "103/2"},
