7edf137b4d39bf082d258b6f85c58e7bd266458a
Yara-senpai <chalchinskiy@gmail.com>
2023-12-05 00:26:54 +0200

    Add files via upload

---
+++ backend/Settings/DormResidentSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Student.Settings
+{
+    public class DormResidentSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string CollectionName { get; set; } = "DormResidents";
+    }
+}
---
+++ backend/Settings/StudentsDatabaseSetings.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Student.Settings
+{
+    public class StudentsDatabaseSetings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string CollectionName { get; set; } = "E-Students";
+    }
+}
