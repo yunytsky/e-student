ac26127b9ecb6d3f26baea411131c7eb1c835202
Yara-senpai <chalchinskiy@gmail.com>
2023-12-05 00:36:59 +0200

    Add files via upload

--- backend/Models/DormResidentModel.cs
+++ backend/Models/DormResidentModel.cs
@@ -1,11 +1,27 @@
-﻿namespace E_Student.Models;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace E_Student.Models;
 
 public class DormResidentModel
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ID { get; set; }
+
+    [BsonElement("fullName")]
     public string FullName { get; set; }
+
+    [BsonElement("dormPassNumber")]
     public string DormPassNumber { get; set; }
+
+    [BsonElement("dormAdress")]
     public string DormAddress { get; set; }
+
+    [BsonElement("dormRoom")]
     public string DormRoom { get; set; }
+
+    [BsonElement("dormAccount")]
     public string DormAccount { get; set; }
     
     public string GetFullInfo()
